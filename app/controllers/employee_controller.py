af3326d15801f041499e6a59e6a3613506d6267e
Nisarg Soni <nisarg.soni@smartsensesolutions.com>
2021-06-11 13:39:37 +0530

    update employee_controller.py

--- app/controllers/employee_controller.py
+++ app/controllers/employee_controller.py
@@ -1,14 +1,13 @@
 from flask import Blueprint,request,jsonify
 from app.models.models import Employee
 from app.database.connect import sess
+import json
 
 bp = Blueprint('bp',__name__)
-
 
 @bp.route("/",methods=['GET'])
 def index():
     return "Home Page"
-
 
 @bp.route('/employee/add',methods=['POST'])
 def create():
@@ -26,7 +25,7 @@ def create():
         return jsonify({"OK":"true","data":data}),200
     except Exception as e:
         sess.rollback()
-        return jsonify({"OK":"false","data":{"error":str(e)}}),422
+        return jsonify({"OK":"false","error":str(e),"data":{}}),400
 
 
 @bp.route('/employee/<int:id>/delete',methods=['DELETE'])
@@ -34,36 +33,43 @@ def delete_Employee(id):
     try:
         emp=sess.query(Employee).filter_by(id=id).first()
         if emp==None:
-            return jsonify({"OK":"true","data":{"message":"User Doesn't Exist!"}}),200
+            return jsonify({"OK":"true","error":"User Doesn't Exist!","data":{}}),422
         sess.delete(emp)
         sess.commit()
-        return jsonify({"OK":"true","data":{"message":"Data Deleted!"}}),200
+        return jsonify({"OK":"true","message":"Data Deleted!","data":{}}),200
     except Exception as e:
         sess.rollback()
-        return jsonify({"OK":"false","data":{"error":str(e)}}),422
-
+        return jsonify({"OK":"false","error":str(e),"data":{}}),400
 
 
 @bp.route('/employee/<int:id>/update',methods=['PUT'])
 def update_data(id):
     try:
         data=request.get_json()
+
+        emp=sess.query(Employee).filter_by(id=id).first()
+        if emp==None:
+            return jsonify({"OK":"true","error":"User Doesn't Exist!","data":{}}),422
+
         sess.query(Employee).filter_by(id=id).update(data)
         sess.commit()
         return jsonify({"OK":"true","data":data,"message":"Data Updated!"}),200
     except Exception as e:
         sess.rollback()
-        return jsonify({"OK":"false","data":{"error":str(e)}}),422
+        return jsonify({"OK":"false","error":str(e),"data":{}}),400
 
 @bp.route('/employee/<int:id>/fetch',methods=['GET'])
 def fetch_data(id):
     try:
         emp=sess.query(Employee).filter_by(id=id).first()
         if emp==None:
-            return jsonify({"OK":"true","data":{"message":"User Doesn't Exist!"}}),200
-        return {"id":id,"OK":"true","data":{"title":emp.title,"dept":emp.department,"country":emp.country,"state":emp.state,"city":emp.city,"remote":emp.remote,"description":emp.description,"requirements":emp.requirements,"benifit":emp.benifits,"comind":emp.company_industry,"job_fun":emp.job_function,"employment_type":emp.employment_type,"experience":emp.experience,"education":emp.education,"keywords":emp.keywords,"salaryFrom":emp.salary_from,"salaryTo":emp.salary_to,"currency":emp.currency}}
+            return jsonify({"OK":"true","error":"User Doesn't Exist!","data":{}}),422
+        
+        row2dict = lambda emp: {c.name: str(getattr(emp, c.name)) for c in emp.__table__.columns}
+        return jsonify({"OK":"true","data":row2dict(emp)}),200
+        
     except Exception as e:
-        return jsonify({"OK":"false","data":{"error":str(e)}}),422
+        return jsonify({"OK":"false","error":str(e),"data":{}}),400
 
 
 @bp.route('/employee/fetchall',methods=['GET'])
@@ -71,7 +77,7 @@ def fetch():
     try:
         emp=sess.query(Employee).all()
         if len(emp)==0:
-            return jsonify({"OK":"true","data":{"Message":"Empty Database"}}),200
-        return jsonify({"OK":"true","data":[i.title for i in emp]}),200
+            return jsonify({"OK":"true","Message":"Empty Database","data":{}}),200
+        return jsonify({"OK":"true","data":[{"title":i.title,"id":i.id} for i in emp]}),200
     except Exception as e:
-        return jsonify({"OK":"false","data":{"error":str(e)}}),422
+        return jsonify({"OK":"false","error":str(e),"data":{}}),400
